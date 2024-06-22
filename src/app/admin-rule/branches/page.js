b36679f4018d56fd2db3da8d7ed75af10e0e2682
Said <saidemad098@gmail.com>
2024-06-22 22:56:54 +0300

    edit page , new page and fix bugs

--- src/app/admin-rule/branches/page.js
+++ src/app/admin-rule/branches/page.js
@@ -1,42 +1,85 @@
-import React from 'react'
+"use client"
+import React , {useState} from 'react'
 import '../input.css'
 import { IoMdAdd } from "react-icons/io";
 import { MdEdit , MdDelete } from "react-icons/md";
+import swal from 'sweetalert2';
+import Swal from 'sweetalert2';
 
 
 
-export default function Home() {
-    return (
 
+const Home = () => {
+
+   const [drop , setDrop] = useState(false);
+
+   return (
+<>
         
-       <div>
-         <nav className="bg-gray-50 w-full mb-10 outline-none">
-            <div className="flex items-center justify-center" >
+      <div>
+        <nav className="bg-gray-50 w-full mb-10 outline-none">
+           <div className="flex items-center justify-center" >
 
-            <div className='flex items-center bg-green-700 hover:bg-green-800 m-2 text-white'><IoMdAdd className='ml-2 text-2xl font-bold'/>
-            <a href = "#" className='p-2 text-white'>new branche</a>
+           <div className='flex items-center bg-green-700 hover:bg-green-800 m-2 text-white'><IoMdAdd className='ml-2 text-2xl font-bold'/>
+           <a href = "./new-branche" className='p-2 text-white'>new branche</a>
 
-            </div>
-          
-            </div>
-         </nav>
+           </div>
+         
+           </div>
+        </nav>
 
-         <div className='flex justify-between items-center bg-gray-100 p-4 mr-5 ml-5'>
-            <h2>Main Branche</h2>
-            <h2>01002819302</h2>
-            <h2>El agamy kilo 21</h2>
 
-            <div className='flex items-center justify-center'>
-            <a href='./edit-branche' className='text-green-500 mr-2'><MdEdit/></a>
-            <a href='' className='text-red-500'><MdDelete/></a>
-            </div>
 
-         </div>
-        
-        
+
+
+        <div className='flex justify-between bg-gray-100 items-center p-4 mr-5 ml-5 hover:animate-pulse delay-150 hover:border-green-500 border-b-2 duration-300'
+       
+       onClick={()=>{
+
+         
+      
+
+       }}
+
+       >
+           <h2 className='items-center'>Main Branche</h2>
+           <h2 className='items-center'>01002819302</h2>
+           <h2 className='items-center'>El agamy kilo 21</h2>
+
+           <div className='flex items-center justify-center'>
+           <a href='./edit-branche' className='text-green-500 mr-2'><MdEdit/></a>
+           <a href='' className='text-red-500'><MdDelete/></a>
+           </div>
+
+
+
+
+           
+
         </div>
 
 
 
-    );
+    
+
+     
+        
+
+
+
+
+
+      
+
+
+       
+       
+       </div>
+</>
+
+
+   );
+
 }
+
+export default Home;
--- src/app/admin-rule/new-branche/page.js
+++ src/app/admin-rule/new-branche/page.js
@@ -35,10 +35,10 @@ export default function Home () {
 
           <div className="flex">
             <h2 className='text-blue-500 font-extrabold'>Branche Name</h2>
-            <input type="text" name="name" placeholder="Main Branche" className=" outline-none ml-2 text-sm"/>
+            <input type="text" name="name" placeholder="Enter Branche Name" className=" outline-none ml-2 text-sm"/>
 
             <h2 className='text-blue-500 font-extrabold'>phone Number</h2>
-            <input type="number" name="phone" placeholder="+966254123654" className=" outline-none text-sm ml-2"/>
+            <input type="number" name="phone" placeholder="Enter Phone Number" className=" outline-none text-sm ml-2"/>
 
 
           </div>
@@ -47,9 +47,9 @@ export default function Home () {
 
     <div>
 
-        <div className='flex flex-col item-center mb-5 justify-center'>
+        <div className='flex flex-col item-center mb-10 justify-center'>
         <h2 className='text-blue-500 font-extrabold'>Location</h2>
-        <input type="text" name="location" placeholder="Riyadh , 35 abou gaber street" className=" outline-none text-center text-sm flex-1 ml-2"/>
+        <input type="text" name="location" placeholder="Enter your Branche Location" className=" outline-none text-center text-sm flex-1 ml-2"/>
         </div>
     
     </div>
@@ -58,15 +58,45 @@ export default function Home () {
 
         <div className='flex flex-col item-center mb-5 justify-center'>
         <h2 className='text-blue-500 font-extrabold'>City</h2>
-        <input type="text" name="city" placeholder="Riyadh" className=" outline-none text-center text-sm flex-1 ml-2"/>
+        <input type="text" name="city" placeholder="Enter your Branche City" className=" outline-none text-center text-sm flex-1 ml-2"/>
         </div>
     
     </div>
 
 
+    <div className="flex flex-col">
+    <h2 className='text-blue-500 font-extrabold mb-2'>select kitchens</h2>
+    
+    <div className="flex justify-between items-center mb-5">
+
+        <div className="flex flex-col items-center justify-center">
+        <Image className="mr-2 mb-2" src= {sweet} width={40}/>
+        <label className="flex item-center text-xs"><input type="checkbox" name="sweet" className="mr-1"/>Sweet</label>
+        </div>
+
+        <div className="flex flex-col items-center justify-center">
+        <Image className="mr-2 mb-2" src= {food} width={40}/>
+        <label className="flex item-center text-xs"><input type="checkbox" name="foods" className="mr-1"/>Foods</label>
+        </div>
+
+        <div className="flex flex-col items-center justify-center">
+        <Image className="mr-2 mb-2" src= {drink} width={40}/>
+        <label className="flex item-center text-xs"><input type="checkbox" name="drinks" className="mr-1"/>Drinks</label>
+        </div>
+
+    </div>
+    </div>
 
 
-
+    <div className="flex flex-col">
+    <h2 className='text-blue-500 font-extrabold mb-2'>select zone</h2>
+    
+    <div className="flex justify-between items-center mb-5">
+        <label className="flex item-center text-xs"><input type="checkbox" name="outdoor" className="mr-1"/>Outdoor</label>
+        <label className="flex item-center text-xs"><input type="checkbox" name="indoor" className="mr-1"/>Indoor</label>
+        <label className="flex item-center text-xs"><input type="checkbox" name="upstairs" className="mr-1"/>Upstairs</label>
+    </div>
+    </div>
 
 
     <div className="flex flex-col items-center mb-2">
@@ -91,7 +121,7 @@ export default function Home () {
       </>
       }
     }
-    >Update
+    >Save
     </a>
 
     </div>
